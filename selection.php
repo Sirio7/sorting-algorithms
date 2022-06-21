24d02228954b9a583c357c7cc5288d546a3eea9f
Sirio7 <giuseppe.distefano88@gmail.com>
2022-06-21 15:33:43 +0200

    Selection Sort

--- selection.php
+++ selection.php
@@ -1,4 +1,27 @@
 <div>
     <h2>Selection Sort</h2>
-    <?php ?>
+    <p>
+        <?php
+        $numbers = [34, 72, 25, 1, 5, 6, 9];
+        $n = count($numbers);
+
+        foreach ($numbers as $number) {
+            echo $number . ' ';
+        }
+        ?>
+    </p>
+    <?php
+    for($i=0;$i<$n-1;$i++){
+        for($j=$i;$j<$n-1;$j++){
+            if($numbers[$i]>$numbers[$j+1]){
+                $tmp = $numbers[$i];
+                $numbers[$i] = $numbers[$j+1];
+                $numbers[$j+1] = $tmp;
+            }
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . ' ';
+    }
+    ?>
 </div>
